d21c290564c46bc54db084c6af8851c855c8c207
DaryaBorovinskaya <dashh17@gmail.com>
2024-02-28 13:08:00 +0700

    Add interface IMenuConfigReader.cs

---
+++ ModernMetadata/Model/Metadata/Readers/IMenuConfigReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernMetadata.Model.Metadata.Readers
+{
+    public interface IMenuConfigReader
+    {
+        public IMenuData ReadMenuData(IUserMenuData userMenuData);
+    }
+}
